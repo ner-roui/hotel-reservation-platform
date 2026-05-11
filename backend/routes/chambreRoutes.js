ea8ca90460c8ee09dbe8dba2b58cd05d0613c7d2
ner-roui <ner-roui@DESKTOP-AAHHVUE.localdomain>
2026-05-11 16:51:10 +0100

    getallroom route

--- backend/routes/chambreRoutes.js
+++ backend/routes/chambreRoutes.js
@@ -4,9 +4,26 @@ const express = require("express");
 const router = express.Router();
 
 const upload = require("../middleware/multer");
-const { addRoom } = require("../controllers/chambre");
+const {addRoom ,
+        getAllRooms,
+        getRoomById,
+        getAvailableRooms,
+} = require("../controllers/chambre");
 
 // ADD ROOM
 router.post("/add-room",upload.array("images", 10),addRoom);
+
+
+
+// GET all
+router.get("/", getAllRooms);
+
+// GET available
+router.get("/available", getAvailableRooms);
+
+
+// GET by id
+router.get("/:id", getRoomById);
+
 
 module.exports = router;
