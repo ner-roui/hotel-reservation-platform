92499eea37bb6ca586f97f85fea3f5ec09748344
ner-roui <ner-roui@DESKTOP-AAHHVUE.localdomain>
2026-05-11 19:14:00 +0100

    modification dans api register , stocker le token dans cookies par le backend au lieu de l envoyer vers le frontend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,18 +2,18 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-exports.register = async (req, res) => {
+register = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
 
-    // 1. validation
+    // validation
     if (!name || !email || !password) {
       return res.status(400).json({
         message: "Tous les champs sont obligatoires",
       });
     }
 
-    // 2. check user exists
+    // check user
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
@@ -22,10 +22,10 @@ exports.register = async (req, res) => {
       });
     }
 
-    // 3. hash password
+    // hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // 4. create user
+    // create user
     const user = await User.create({
       name,
       email,
@@ -33,12 +33,20 @@ exports.register = async (req, res) => {
       role,
     });
 
-    // 5. generate token
+    // create token
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET,
       { expiresIn: "7d" }
     );
+
+    //  STORE TOKEN IN COOKIE
+    res.cookie("token", token, {
+      httpOnly: true,        //  impossible à accéder via JS frontend
+      secure: false,         // true en production (HTTPS)
+      sameSite: "lax",       // protection CSRF basique
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
+    });
 
     return res.status(201).json({
       message: "Utilisateur créé avec succès",
@@ -48,8 +56,8 @@ exports.register = async (req, res) => {
         email: user.email,
         role: user.role,
       },
-      token,
     });
+
   } catch (error) {
     console.error(error);
     return res.status(500).json({
@@ -57,3 +65,6 @@ exports.register = async (req, res) => {
     });
   }
 };
+
+
+module.exports = {register }
