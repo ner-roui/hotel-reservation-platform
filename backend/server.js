76b9d32db853e33f46fa99c144cb9da2bdd88d6b
errouihinisrine <errouihi.nisrine@etu.uae.ac.ma>
2026-06-09 17:04:52 +0100

    cors issue fix

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,14 @@ app.use(cors({
 
 // Répondre aux requêtes preflight OPTIONS
 app.options("*", cors());
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://hotel-reservation-platform-one.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
 
 app.use(express.json());
 app.use(cookieParser());
